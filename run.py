9cef1c6cb79cb7ac5e2dab590164f5ffe07d6d7f
Tommy-83 <>
2023-05-05 17:15:02 +0000

    last commit before trying to migrate to heroku

--- run.py
+++ run.py
@@ -7,29 +7,34 @@ def read_random_word():
         word_array = f.read().splitlines()
         return random.choice(word_array)
 
-print("Let's play Wordle:")
-print("Type a 5 letter word below and press Enter. You have 6 tries to guess the random word.\n")
+def print_menu():
+    print("Let's play Wordle:")
+    print("Type a 5 letter word below and press Enter. You have 6 tries to guess the random word.\n")
 
-word = read_random_word()
 
-for attempt in range(1,7):
-    guess = input().lower()
+print_menu()
+play_again = ""
+while play_again != "q":
+    word = read_random_word()
     
-    # overwrite the last line in the console
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
+    for attempt in range(1, 7):
+        guess = input().lower()
 
-    # print colored letters
-    for i in range(min(len(guess), 5)):
-        if guess[i] == word[i]:
-            print(colored(guess[i], 'green'), end="")
-        elif guess[i] in word:
-            print(colored(guess[i], 'yellow'), end="")
-        else:
-            print(guess[i], end="")
-    print()
-    
-    if guess == word:
-        print(f"Congratulations! You guessed the word in {attempt} guesses.", 'green')
-    elif attempt == 6:
-        print(f"You didn't guess the word within 6 tries, it was {word}")
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+
+        for i in range( min(len(guess), 5) ):
+            if guess[i] == word[i]:
+                print(colored(guess[i], 'green'), end="")
+            elif guess[i] in word:
+                print(colored(guess[i], 'yellow'), end="")
+            else:
+                print(guess[i], end="")
+        print()
+
+        if guess == word:
+            print(colored(f"Congrats! You got the wordle in {attempt}", 'red'))
+            break
+        elif attempt == 6:
+            print(f"Sorry the wordle was.. {word}")
+    play_again = input("Want to play again? Type q to exit.")
