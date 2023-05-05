4471dccfd9b687487bcac866a0c37253f621a6b5
Tommy-83 <>
2023-05-05 13:33:56 +0000

    adding colors to selected letters of words

--- run.py
+++ run.py
@@ -13,3 +13,14 @@ def read_random_word():
 
 print_menu()
 word = read_random_word()
+
+for a attempt in range(1, 7)
+    guess = input().lower()
+
+    for i in range( min(len(guess), 5) ):
+        if guess[i] == word[i]:
+            print(colored(guess[i], 'green'), end="")
+        elif guess[i] in word:
+            print(colored(guess[i], 'yellow'), end="")
+        else:
+            print(guess[i], end="")
