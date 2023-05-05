32835fa8f25669101dac73e8bfd0dd896818ccc3
Tommy-83 <>
2023-05-05 13:13:55 +0000

    adding random function

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+import random
+
 def print_menu():
     print("Welcome to Wordle")
     print("Type 5 letter word and hit enter")
 
+def read_random_word():
+    with open("words.txt") as f:
+        words = f.read().splitlines()
+        return
+
 print_menu()
+
