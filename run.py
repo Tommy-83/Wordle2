b147ebcbb59a622689423f9e2490e45162d9e213
Tommy-83 <>
2023-05-05 14:33:53 +0000

    adding words to the words.txt file

--- run.py
+++ run.py
@@ -12,25 +12,26 @@ def read_random_word():
         return random.choice(words)
 
 print_menu()
-word = read_random_word()
+play_again = ""
+while play_again != "q":
+    word = read_random_word()
+    for attempt in range(1, 7):
+        guess = input().lower()
 
-for attempt in range(1, 7):
-    guess = input().lower()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[1K')
 
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[1K')
+        for i in range( min(len(guess), 5) ):
+            if guess[i] == word[i]:
+                print(colored(guess[i], 'green'), end="")
+            elif guess[i] in word:
+                print(colored(guess[i], 'yellow'), end="")
+            else:
+                print(guess[i], end="")
+        print()
 
-    for i in range( min(len(guess), 5) ):
-        if guess[i] == word[i]:
-            print(colored(guess[i], 'green'), end="")
-        elif guess[i] in word:
-            print(colored(guess[i], 'yellow'), end="")
-        else:
-            print(guess[i], end="")
-    print()
-
-    if guess == word:
-        print(colored(f"Congrats! YOu got the wordle in {attempt}", 'red'))
-        break
-
+        if guess == word:
+            print(colored(f"Congrats! YOu got the wordle in {attempt}", 'red'))
+            break
+    play_again = input("Want to play again? Press q to exit.")
         
