6d8e4dff853f7a1f91da68c9364ff72024753b5b
Tommy-83 <>
2023-05-05 16:52:15 +0000

    managed to delete the code so added it slowly back again...

--- run.py
+++ run.py
@@ -1,40 +1,35 @@
+from termcolor import colored
 import random
 import sys
-from termcolor import colored, cprint
-
-def print_menu():
-    print("Welcome to Wordle")
-    print("Type 5 letter word and hit enter\n")
 
 def read_random_word():
     with open("words.txt") as f:
-        words = f.read().splitlines()
-        return random.choice(words)
+        word_array = f.read().splitlines()
+        return random.choice(word_array)
 
-print_menu()
-play_again = ""
-while play_again != "q":
-    word = read_random_word()
-    for attempt in range(1, 7):
-        guess = input().lower()
+print("Let's play Wordle:")
+print("Type a 5 letter word below and press Enter. You have 6 tries to guess the random word.\n")
 
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[1K')
+word = read_random_word()
 
-        for i in range( min(len(guess), 5) ):
-            if guess[i] == word[i]:
-                print(colored(guess[i], 'green'), end="")
-            elif guess[i] in word:
-                print(colored(guess[i], 'yellow'), end="")
-            else:
-                print(guess[i], end="")
-        print()
+for attempt in range(1,7):
+    guess = input().lower()
+    
+    # overwrite the last line in the console
+    sys.stdout.write('\x1b[1A')
+    sys.stdout.write('\x1b[2K')
 
-        if guess == word:
-            print(colored(f"Congrats! YOu got the wordle in {attempt}", 'red'))
-            break
-        elif attempt == 6:
-            print(f"Sorry the word was {word}")
-
-    play_again = input("Want to play again? Press q to exit.")
-        
+    # print colored letters
+    for i in range(min(len(guess), 5)):
+        if guess[i] == word[i]:
+            print(colored(guess[i], 'green'), end="")
+        elif guess[i] in word:
+            print(colored(guess[i], 'yellow'), end="")
+        else:
+            print(guess[i], end="")
+    print()
+    
+    if guess == word:
+        print(f"Congratulations! You guessed the word in {attempt} guesses.", 'green')
+    elif attempt == 6:
+        print(f"You didn't guess the word within 6 tries, it was {word}")
